79293b155080c66517f2b559f7cb7a722a40c1b6
fr-kfukuda <keito.fukuda@fastretailing.com>
2015-07-18 11:34:19 +0900

    display realtime total user count and give each single user their own ID

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var user_count = 0;
+var sequence_number = 0;
+
 io.on('connection', function(socket){
-  socket.emit('chat message', 'Welcome to chat room. Have fun here.');
-  socket.broadcast.emit('chat message', 'A user newly joined.');
+  user_count++;
+  sequence_number++;
+  var user_id = 'User_ID_'+sequence_number;
+
+  socket.emit('chat message', 'Welcome to chat room. Your User_ID is '+user_id+'. Have fun here.');
+  socket.broadcast.emit('chat message', user_id+' just connected.');
+  io.emit('user count', user_count);
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
   socket.on('disconnect', function(){
-    io.emit('chat message', 'A user disconneted.');
+    user_count--;
+    io.emit('chat message', user_id+' disconneted.');
+    io.emit('user count', user_count);
   });
 });
 
